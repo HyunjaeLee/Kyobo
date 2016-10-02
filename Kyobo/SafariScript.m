dcdf54ad12d947919a83d397cf40410e85eb2777
Hyunjae <guswoqkrtk@gmail.com>
2016-10-02 22:16:02 +0900

    Commit 2

--- Kyobo/SafariScript.m
+++ Kyobo/SafariScript.m
@@ -13,15 +13,23 @@
 - (void) run {
     
     SafariApplication *safari = [SBApplication applicationWithBundleIdentifier:@"com.apple.Safari"];
-    SBElementArray* documents = [safari documents];
-    SafariDocument* document = [documents objectAtIndex: 0];
+    SBElementArray *documents = [safari documents];
+    SafariDocument *newDocument = [[[safari classForScriptingClass:@"document"] alloc] init];
+    [documents addObject: newDocument];
+    SafariDocument *document = [documents objectAtIndex: 0];
     [document setURL: @"http://www.kyobobook.co.kr/login/login.laf?retURL=http%3A//www.kyobobook.co.kr/prom/2015/book/151116_dailyCheck.jsp%3ForderClick%3Dc1j"];
-    sleep(3);
+    while([safari doJavaScript:@"document.getElementsByClassName('btn_submit')[0].innerHTML" in: (SafariTab *) document] == nil){
+        sleep(1);
+    }
     [safari doJavaScript:@"document.getElementById('memid').value = 'guswoqkrtk'" in: (SafariTab *) document];
     [safari doJavaScript:@"document.getElementById('pw').value = '4c8cc5f13f'" in: (SafariTab *) document];
     [safari doJavaScript:@"document.getElementsByClassName('btn_submit')[0].click()" in: (SafariTab *) document];
-    sleep(3);
+    while([safari doJavaScript:@"document.getElementsByClassName('likeit')[0].innerHTML" in: (SafariTab *) document] == nil){
+        sleep(1);
+    }
     [safari doJavaScript:@"goSubmit('001');" in: (SafariTab *) document];
+    sleep(3);
+    [documents removeObject: document];
     
 }
 
